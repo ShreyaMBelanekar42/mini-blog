5de7d5ad1bc292a7d2596a0d57d00a0c4be5b314
ShreyaMBelanekar42 <shreyaluvbelanekar@gmail.com>
2022-12-14 13:40:11 +0530

    added loading and error messages

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,20 +3,35 @@ import Bloglist from "./Bloglist";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [loading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
       .then(res => {
+        if(res.ok !== true){
+          throw Error('Could not fetch the data!!')
+        }
         return res.json();
       })
       .then(data => {
         console.log(data);
         setBlogs(data);
+        setIsLoading(false);
+        setError(null);
       })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      })
+    }, 1000);
   }, []);
 
   return (
     <>
+      {error && <div className="error-text">{error}</div>}
+      {loading && <div className="loading-text">Loading...</div>}
       {blogs && <Bloglist blogs={blogs} title="All Blogs"/>}
     </>
   );
